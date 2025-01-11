0ffe287a4615164e3e66a26a52a39d9fb08d6bb1
Sumeet Deorukhkar <sumeetdeorukhkar@outlook.com>
2025-01-11 22:15:05 +0530

    new deploy

--- mentalhealthapi/Migrations/20250111164429_DoctorDeploy.Designer.cs
+++ mentalhealthapi/Migrations/20250111164429_DoctorDeploy.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mentalhealthapi.Data;
 
@@ -11,9 +12,11 @@ using mentalhealthapi.Data;
 namespace mentalhealthapi.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250111164429_DoctorDeploy")]
+    partial class DoctorDeploy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ mentalhealthapi/Migrations/20250111164429_DoctorDeploy.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mentalhealthapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class DoctorDeploy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Doctors",
+                type: "nvarchar(1)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Doctors",
+                type: "nvarchar(1)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)");
+        }
+    }
+}
